041b597bfaf9a1d051779d523e9de3f92e3f51dc
mathilde.ferraguti@hotmail.fr <107366266+tilda1234@users.noreply.github.com>
2024-08-19 11:40:13 +0200

    api.php

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,10 @@ use Illuminate\Support\Facades\Route;
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
+
+Routes defined in the routes/api.php file are nested within a route group by the RouteServiceProvider. 
+Within this group, the /api URI prefix is automatically applied 
+so you do not need to manually apply it to every route in the file.
 |
 */
 
